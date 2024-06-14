ebed8892bc49eec8d355b567777f6d209d82fc96
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-06-14 21:34:49 +1000

    Nearly finished Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css';
+import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className={styles.App}>
     <Navbar />
+    <Hero />
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import styles from './Hero.module.css';
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+    <div className={styles.content}>
+    <h1 className={styles.title}>Hi, I&apos;m Kimleng Thai!</h1>
+    <p className={styles.description}>I&apos;m a software engineer. I love to build things and I like coding.</p>
+    <a href="mailto:kimleng.ngs@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    </div>
+    <img src="../../../assets/profile/profilePicture.jpeg" alt="My profile image" className={styles.heroImg}/>
+    <div className={styles.topBlur}></div>
+    <div className={styles.bottomBlur}></div>
+    </section>
+  )
+}
