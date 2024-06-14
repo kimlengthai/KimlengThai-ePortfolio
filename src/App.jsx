096f2634691a15bbc997619ca0394e701bbd4a08
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-06-15 01:44:24 +1000

    Basic structure of About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css';
 import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
+import { About } from './components/About/About';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className={styles.App}>
     <Navbar />
     <Hero />
+    <About />
     </div>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,51 @@
+import styles from './About.module.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export const About = () => {
+  return (
+    <section className={styles.aboutSection} id="about">
+      <h2>Welcome to my page!</h2>
+      <div className={styles.aboutContent}>
+        <img
+          src="../../../assets/standup/standupImg.png"
+          alt="My standard picture"
+          className={styles.aboutImage}
+        />
+        <p className={styles.introText}>About Me</p>
+        <p className={styles.aboutText}>I am a software engineering student at the University of Technology Sydney, 
+        passionate about learning new skills and undertaking new projects. I constantly seek opportunities to enhance 
+        my expertise and contribute to impactful solutions. Through various projects, I have demonstrated my passion 
+        for problem-solving and my skill in front-end development. My hobbies include playing sports and traveling. 
+        I have proven myself to have excellent communication and teamwork skills, enjoying meaningful conversations 
+        that contribute to a positive and collaborative environment.
+        </p>
+        <div className={styles.buttons}>
+          <a href="mailto:your-email@example.com" className={styles.contactButton}>
+            Contact me
+          </a>
+          <a href="https://www.linkedin.com/in/your-profile" className={styles.connectButton} target="_blank" rel="noopener noreferrer">
+            Connect with me
+          </a>
+        </div>
+        <div className={styles.navigation}>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#experience">Experience</a>
+          <a href="#project">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+        <div className={styles.icons}>
+          <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer">
+            <GitHubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+            <LinkedInIcon />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
