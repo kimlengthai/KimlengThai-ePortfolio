6d4e78f853fba776b5150fbbc9d9acfa11f09e5d
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2025-06-07 23:41:08 +1000

    Add Work Experience + Skill + Navigation

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,22 +3,23 @@ import htmlSkill from '../../../assets/skills/html.png';
 import cssSkill from '../../../assets/skills/css.png';
 import tailwindcssSkill from '../../../assets/skills/tailwindCSS.png';
 import javascriptSkill from '../../../assets/skills/javascript.png';
-import sqlSkill from '../../../assets/skills/sql.png';
 import reactSkill from '../../../assets/skills/react.png';
 import vueJSSkill from '../../../assets/skills/vueJS.png';
 import nextJSSkill from '../../../assets/skills/nextJS.png';
-import javaFXSkill from '../../../assets/skills/JavaFX.png';
 import viteSkill from '../../../assets/skills/vite.jpeg';
 import expressJSSkill from '../../../assets/skills/expressJS.png';
+import javaFXSkill from '../../../assets/skills/JavaFX.png';
 import reactNativeSkill from '../../../assets/skills/ReactNative.png';
 import materialUISkill from '../../../assets/skills/materialUI.png';
 import expoSkill from '../../../assets/skills/expo.png';
 import figmaSkill from '../../../assets/skills/figma.png';
+import awsSkill from '../../../assets/skills/aws_logo.png';
 import cSharpSkill from '../../../assets/skills/cSharp.png';
 import cPlusPlusSkill from '../../../assets/skills/cPlusPlus.png';
 import cSkill from '../../../assets/skills/c.png';
 import javaSkill from '../../../assets/skills/java.png';
 import jspSkill from '../../../assets/skills/JSP.png';
+import sqlSkill from '../../../assets/skills/sql.png';
 import firebaseSkill from '../../../assets/skills/firebase.png';
 import mongodbSkill from '../../../assets/skills/mongoDB.png';
 import jdbcSkill from '../../../assets/skills/JDBC.png';
@@ -26,6 +27,7 @@ import LoginPageImage from '../../../assets/history/LoginPage.png';
 import zambarkImage from '../../../assets/history/zambark.png';
 import mmsImage from '../../../assets/history/mms.png';
 import IoTLoginPageImage from '../../../assets/projects/IoTLoginPage.png';
+import viCyberLogo from '../../../assets/history/vicyber_logo.jpeg';
 
 export const Experience = () => {
   return (
@@ -61,12 +63,6 @@ export const Experience = () => {
             </div>
             <div className={styles.skill}>
               <div className={styles.skillImageContainer}>
-                <img src={sqlSkill} alt="SQL" className={styles.skillImage} />
-              </div>
-              <p>SQL</p>
-            </div>
-            <div className={styles.skill}>
-              <div className={styles.skillImageContainer}>
                 <img src={reactSkill} alt="React" className={styles.skillImage} />
               </div>
               <p>React</p>
@@ -91,6 +87,12 @@ export const Experience = () => {
             </div>
             <div className={styles.skill}>
               <div className={styles.skillImageContainer}>
+                <img src={javaFXSkill} alt="JavaFX" className={styles.skillImage} />
+              </div>
+              <p>JavaFX</p>
+            </div>
+            <div className={styles.skill}>
+              <div className={styles.skillImageContainer}>
                 <img src={reactNativeSkill} alt="React Native" className={styles.skillImage} />
               </div>
               <p>React Native</p>
@@ -109,12 +111,6 @@ export const Experience = () => {
             </div>
             <div className={styles.skill}>
               <div className={styles.skillImageContainer}>
-                <img src={javaFXSkill} alt="JavaFX" className={styles.skillImage} />
-              </div>
-              <p>JavaFX</p>
-            </div>
-            <div className={styles.skill}>
-              <div className={styles.skillImageContainer}>
                 <img src={viteSkill} alt="Vite" className={styles.skillImage} />
               </div>
               <p>Vite</p>
@@ -124,6 +120,12 @@ export const Experience = () => {
                 <img src={figmaSkill} alt="Figma" className={styles.skillImage} />
               </div>
               <p>Figma</p>
+            </div>
+            <div className={styles.skill}>
+              <div className={styles.skillImageContainer}>
+                <img src={awsSkill} alt="AWS" className={styles.skillImage} />
+              </div>
+              <p>AWS</p>
             </div>
             <div className={styles.skill}>
               <div className={styles.skillImageContainer}>
@@ -154,6 +156,12 @@ export const Experience = () => {
                 <img src={jspSkill} alt="JSP" className={styles.skillImage} />
               </div>
               <p>JSP</p>
+            </div>
+            <div className={styles.skill}>
+              <div className={styles.skillImageContainer}>
+                <img src={sqlSkill} alt="SQL" className={styles.skillImage} />
+              </div>
+              <p>SQL</p>
             </div>
             <div className={styles.skill}>
               <div className={styles.skillImageContainer}>
@@ -203,9 +211,29 @@ export const Experience = () => {
                 </div>
               </a>
             </li>
+            {/* Internship Experience */}
+            <li className={styles.historyItem}>
+              <div className={styles.historyLink}>
+                <img
+                  src={viCyberLogo}
+                  alt="ViCyber Logo"
+                  className={styles.historyImage}
+                />
+                <div className={styles.historyItemDetails}>
+                  <h4>Software Engineering Intern, ViCyber</h4>
+                  <p>Nov, 2024 - Feb, 2025</p>
+                  <ul>
+                    <li>Developed and optimised frontend components using React.js to enhance user experience</li>
+                    <li>Automated dynamic PDF report generation using Puppeteer, AWS Lambda, and structured JSON</li>
+                    <li>Designed 20+ responsive UI/UX components in Figma to support cross-device usability</li>
+                    <li>Collaborated on AWS-based infrastructure, improving performance across key workflows</li>
+                  </ul>
+                </div>
+              </div>
+            </li>
             <li className={styles.historyItem}>
               <a
-                href="https://github.com/kimlengthai/BabyUniversity" // Replace with actual URL for LoginPage
+                href="https://github.com/kimlengthai/BabyUniversity"
                 target='_blank'
                 rel='noopener noreferrer'
                 className={styles.historyLink}
@@ -227,7 +255,7 @@ export const Experience = () => {
             </li>
             <li className={styles.historyItem}>
               <a
-                href="https://github.com/kimlengthai/ZAMBARK-SUGGEST" // Replace with actual URL for zambark
+                href="https://github.com/kimlengthai/ZAMBARK-SUGGEST"
                 target='_blank'
                 rel='noopener noreferrer'
                 className={styles.historyLink}
@@ -249,7 +277,7 @@ export const Experience = () => {
             </li>
             <li className={styles.historyItem}>
               <a
-                href="https://github.com/kimlengthai/MembershipManagementSystem" // Replace with actual URL for mms
+                href="https://github.com/kimlengthai/MembershipManagementSystem"
                 target='_blank'
                 rel='noopener noreferrer'
                 className={styles.historyLink}
@@ -272,6 +300,14 @@ export const Experience = () => {
           </ul>
         </div>
       </div>
+
+      <nav className={styles.navigation} aria-label="Secondary navigation">
+              <a href="#home">Home</a>
+              <a href="#about">About</a>
+              <a href="#experience">Experience</a>
+              <a href="#project">Projects</a>
+              <a href="#contact">Contact</a>
+        </nav>
     </section>
   );
 }
